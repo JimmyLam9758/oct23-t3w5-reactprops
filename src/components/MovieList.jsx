e3ccbea467d8ef8a36c046c494d6471b4bc43aff
JimmyLam9758 <JimmyLam9758@gmail.com>
2024-05-02 19:51:03 +1000

    fix: connect all components and fix movielist key

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,30 +1,46 @@
+// ES6/ESM/module project import syntax
+import MovieCard from "./MovieCard.jsx";
 
+// CommonJS/NodeJS project import syntax
+// const MovieCard = require("./MovieCard.jsx");
 
 let favouriteMovies = [
-    "Aliens",
-    "Dune",
-    "Hot Fuzz",
-    "Anchorman",
-    "Lock Stock and 2 Smoking Barrels",
-    "Shrek",
-    "Pokemon",
-    "Blazing Saddles",
-    "Snatch",
-    "Shrek 2",
-    "The good, the bad, and the ugly",
-    "Robocop",
-    "Bladerunner",
-    "Lord of the Rings 1-3"
-  ];
+	"Aliens",
+	"Dune",
+	"Hot Fuzz",
+	"Anchorman",
+	"Lock Stock and 2 Smoking Barrels",
+	"Shrek",
+	"Pokemon",
+	"Blazing Saddles",
+	"Snatch",
+	"Shrek 2",
+	"The good, the bad, and the ugly",
+	"Robocop",
+	"Bladerunner",
+	"Lord of the Rings 1-3"
+];
 
 export default function MovieList(){
 
-    return (
-    <section>
-        {favouriteMovies.map((movie, index) => {
-            return <MovieCard key={index} elephant={movie}/>
-        })}
-    </section>
-        
-    );
+	return (
+	<section>
+		{favouriteMovies.map((movie, index) => {
+			return <MovieCard key={movie + "-" + index} elephant={movie} />
+		})}
+	</section>
+	);
 }
+
+
+// export function SomeFunction(){
+
+// }
+
+// module.exports = {
+// 	SomeFunction
+// }
+
+// module.exports = MovieList;
+
+// import MovieList, {SomeFunction} from "thefile "
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,15 +1,21 @@
+import MovieList from '../components/MovieList';
 import '../stylesheets/App.css';
 
 function App() {
 
-
-    //
+  // 
+  // 
 
   return (
     <div className="App">
       <h1>Favourite Movies</h1>
 
+      {/* <MovieList /> */}
+      <MovieList />
       
+
+      {/*  */}
+
     </div>
   );
 }

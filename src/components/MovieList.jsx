b6bcbf7c72d7102fdeaf32618a3b9a7686f45386
JimmyLam9758 <JimmyLam9758@gmail.com>
2024-05-02 19:37:53 +1000

    feat: react app init and organized and movelist component

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,30 @@
+
+
+let favouriteMovies = [
+    "Aliens",
+    "Dune",
+    "Hot Fuzz",
+    "Anchorman",
+    "Lock Stock and 2 Smoking Barrels",
+    "Shrek",
+    "Pokemon",
+    "Blazing Saddles",
+    "Snatch",
+    "Shrek 2",
+    "The good, the bad, and the ugly",
+    "Robocop",
+    "Bladerunner",
+    "Lord of the Rings 1-3"
+  ];
+
+export default function MovieList(){
+
+    return (
+    <section>
+        {favouriteMovies.map((movie, index) => {
+            return <MovieCard key={index} elephant={movie}/>
+        })}
+    </section>
+        
+    );
+}
---
+++ src/pages/App.jsx
@@ -0,0 +1,17 @@
+import '../stylesheets/App.css';
+
+function App() {
+
+
+    //
+
+  return (
+    <div className="App">
+      <h1>Favourite Movies</h1>
+
+      
+    </div>
+  );
+}
+
+export default App;
